b2a31c36dba59bec040048ac5d71146e8e9027b5
romesway <romesway@gmail.com>
2024-05-15 22:13:37 +0900

    필수 기능 구현

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -6,20 +6,61 @@ const router = express.Router();
 // POST: 상품 생성 API
 router.post('/', async (req, res, next) => {
   try {
-    const { name, description, manager, status } = await req.body;
-    const product = new ProductSchema({ name, description, manager, status });
+    const { name, description, manager, password, status } = await req.body;
+    const createdAt = new Date();
+    const updatedAt = new Date();
+    //  dbProduct: {_id: new ObjectId('6..'),name: '수박5',description: 'aa',manager: '옥테인',password: '1155',createdAt: 2024..,updatedAt: 2024.., __v: 0}
+    const dbProduct = await ProductSchema.findOne({ name }).exec();
+    // **** 스키마 unique 처리 안됨. 방법 찾기
+    if (dbProduct) {
+      return res.status(400).json({ message: '이미 등록된 상품입니다.' });
+    }
+    const product = new ProductSchema({
+      name,
+      description,
+      manager,
+      password,
+      status: 'FOR_SALE',
+      createdAt,
+      updatedAt,
+    });
     await product.save();
-    return res.status(201).json(product);
-  } catch {
-    return res.status(404).json({ errorMessage: 'post error' });
+    // **** 리턴 데이터: 생성한 product를 바로 사용 vs 쿼리로 불러와서 사용 / 뭐가 나을까?
+    return res.status(201).json({
+      status: 201,
+      massage: '상품 생성에 성공했습니다.',
+      data: {
+        id: product._id,
+        name,
+        description,
+        manager,
+        status: 'FOR_SALE',
+        createdAt,
+        updatedAt,
+      },
+    });
+  } catch (err) {
+    // **** 에러 메시지에 따라 메시지 처리 필요.
+    return res
+      .status(404)
+      .json({ errorMessage: '상품 정보를 모두 입력해 주세요' });
   }
 });
 
 // GET: 상품 목록 조회 API
 router.get('/', async (req, res, next) => {
   try {
-    const product = await ProductSchema.find().sort('name').exec();
-    return res.status(201).json({ product });
+    // 패스워드 제외 쿼리결과 불러오기, //앞의 {}는 쿼리(전체), 뒤의 {password...}는 프로젝션
+    // 생성 일시 기준으로 내림차순(최신순) 정렬
+    const product = await ProductSchema.find({}, { password: 0, __v: 0 })
+      .sort('-createdAt')
+      .exec();
+    return res.status(201).json({
+      status: 200,
+      massage: '상품 목록 조회에 성공했습니다.',
+      // **** 쿼리 결과에서 필드명 바꿔 불러오는법 확인
+      data: product,
+    });
   } catch {
     return res.status(404).json({ errorMessage: 'search error' });
   }
@@ -29,8 +70,15 @@ router.get('/', async (req, res, next) => {
 router.get('/:productId', async (req, res, next) => {
   try {
     const { productId } = req.params;
-    const product = await ProductSchema.findById(productId).exec();
-    return res.status(201).json(product);
+    const dbProduct = await ProductSchema.findById(productId, {
+      password: 0,
+      __v: 0,
+    }).exec();
+    return res.status(201).json({
+      status: 200,
+      message: '상품 상세 조회에 성공했습니다.',
+      data: dbProduct,
+    });
   } catch {
     return res.status(404).json({ errorMessage: 'detail search error' });
   }
@@ -39,24 +87,39 @@ router.get('/:productId', async (req, res, next) => {
 // UPDATE: 상품 수정 API
 router.patch('/:productId', async (req, res, next) => {
   try {
-    const { name, description, manager, status } = await req.body;
+    const { name, description, manager, status, password } = await req.body;
     const { productId } = req.params;
-    const product = await ProductSchema.findById(productId).exec();
-    const productName = product.name;
+    const dbProduct = await ProductSchema.findById(productId).exec();
+    const dbPassword = dbProduct.password;
+    if (password !== dbPassword) {
+      return res
+        .status(401)
+        .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
+    }
     if (name) {
-      product.name = name;
+      dbProduct.name = name;
     }
     if (description) {
-      product.description = description;
+      dbProduct.description = description;
     }
     if (manager) {
-      product.manager = manager;
+      dbProduct.manager = manager;
     }
     if (status) {
-      product.status = status;
+      dbProduct.status = status;
     }
-    await product.save();
-    return res.status(200).json({ mssage: `${productName} 상품 수정 성공` });
+    dbProduct.updatedAt = new Date();
+    await dbProduct.save();
+    // 업데이트 시간 수정 후 패스워드 제외 db 가져오기 위해 순서 이동
+    const nonPasswordDb = await ProductSchema.findById(productId, {
+      password: 0,
+      __v: 0,
+    }).exec();
+    return res.status(200).json({
+      status: 200,
+      message: '상품 수정에 성공했습니다.',
+      data: nonPasswordDb,
+    });
   } catch {
     return res.status(404).json({ errorMessage: 'update error' });
   }
@@ -65,13 +128,19 @@ router.patch('/:productId', async (req, res, next) => {
 router.delete('/:productId', async (req, res, next) => {
   try {
     const { productId } = req.params;
-    const product = await ProductSchema.findById(productId).exec();
-    const productName = product.name;
-    if (product) {
-      await ProductSchema.deleteOne({ _id: productId });
-      return res.status(200).json({ mssage: `${productName} 상품 삭제 성공` });
+    const dbProduct = await ProductSchema.findById(productId).exec();
+    const { password } = req.body;
+    const dbPassword = dbProduct.password;
+    if (password !== dbPassword) {
+      return res
+        .status(401)
+        .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
     }
-    return res.status(400).json({ errorMessage: '삭제할 상품이 없습니다' });
+    await ProductSchema.deleteOne({ _id: productId });
+    return res.status(200).json({
+      mssage: '상품 삭제에 성공했습니다.',
+      data: { _id: 1 },
+    });
   } catch {
     return res.status(404).json({ errorMessage: 'delete error' });
   }
